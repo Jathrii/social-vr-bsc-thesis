bf7e17eb7be17fcf2654efd6080d36a6378df52b
Omar El-Sherif <omar.ohm.elsherif@gmail.com>
2019-04-15 21:33:33 +0200

    Replace delays in flow control with delete option
    - Remove destroy delay for spwaned messages.
    - Destroy old messages on spawning new ones.
    - Add delete option to destroy the currently spawned message.

--- Assets/Scripts/FlowController.cs
+++ Assets/Scripts/FlowController.cs
@@ -13,11 +13,43 @@ namespace SocialVR
         public GameObject SteamVRPlayer;
         public GameObject SpectatorCamera;
         public static Transform startPosition;
+
+        private KeyCode[] alpha;
+        private KeyCode[] keypad;
+        private InputField[] input;
+        private GameObject message;
+
         // Start is called before the first frame update
         private void Start()
         {
             if (!isServer)
                 return;
+
+            alpha = new[] {
+                KeyCode.Alpha1,
+                KeyCode.Alpha2,
+                KeyCode.Alpha3,
+                KeyCode.Alpha4,
+                KeyCode.Alpha5,
+                KeyCode.Alpha6,
+                KeyCode.Alpha7,
+                KeyCode.Alpha8,
+                KeyCode.Alpha9
+            };
+
+            keypad = new[] {
+                KeyCode.Keypad1,
+                KeyCode.Keypad2,
+                KeyCode.Keypad3,
+                KeyCode.Keypad4,
+                KeyCode.Keypad5,
+                KeyCode.Keypad6,
+                KeyCode.Keypad7,
+                KeyCode.Keypad8,
+                KeyCode.Keypad9
+            };
+
+            input = InfoController.GetComponentsInChildren<InputField>();
 
             SteamVRPlayer.SetActive(false);
             SpectatorCamera.SetActive(true);
@@ -29,58 +61,51 @@ namespace SocialVR
             if (!isServer)
                 return;
 
-            if (Input.GetKeyUp(KeyCode.Alpha1) || Input.GetKeyUp(KeyCode.Keypad1))
+            if (Input.GetKeyDown(KeyCode.Delete))
             {
-                InputField[] info = InfoController.GetComponentsInChildren<InputField>();
-                RpcSpawnInfo(info[0].text, 2.0f);
+                if (message != null)
+                {
+                    Destroy(message);
+                    message = null;
+                }
+
+                RpcSpawnInfo("Delete");
             }
-            else if (Input.GetKeyUp(KeyCode.Alpha2) || Input.GetKeyUp(KeyCode.Keypad2))
+            else
             {
-                InputField[] info = InfoController.GetComponentsInChildren<InputField>();
-                RpcSpawnInfo(info[1].text, 2.0f);
-            }
-            else if (Input.GetKeyUp(KeyCode.Alpha3) || Input.GetKeyUp(KeyCode.Keypad3))
-            {
-                InputField[] info = InfoController.GetComponentsInChildren<InputField>();
-                RpcSpawnInfo(info[2].text, 2.0f);
-            }
-            else if (Input.GetKeyUp(KeyCode.Alpha4) || Input.GetKeyUp(KeyCode.Keypad4))
-            {
-                InputField[] info = InfoController.GetComponentsInChildren<InputField>();
-                RpcSpawnInfo(info[3].text, 2.0f);
-            }
-            else if (Input.GetKeyUp(KeyCode.Alpha5) || Input.GetKeyUp(KeyCode.Keypad5))
-            {
-                InputField[] info = InfoController.GetComponentsInChildren<InputField>();
-                RpcSpawnInfo(info[4].text, 2.0f);
-            }
-            else if (Input.GetKeyUp(KeyCode.Alpha6) || Input.GetKeyUp(KeyCode.Keypad6))
-            {
-                InputField[] info = InfoController.GetComponentsInChildren<InputField>();
-                RpcSpawnInfo(info[5].text, 2.0f);
-            }
-            else if (Input.GetKeyUp(KeyCode.Alpha7) || Input.GetKeyUp(KeyCode.Keypad7))
-            {
-                InputField[] info = InfoController.GetComponentsInChildren<InputField>();
-                RpcSpawnInfo(info[6].text, 2.0f);
-            }
-            else if (Input.GetKeyUp(KeyCode.Alpha8) || Input.GetKeyUp(KeyCode.Keypad8))
-            {
-                InputField[] info = InfoController.GetComponentsInChildren<InputField>();
-                RpcSpawnInfo(info[7].text, 2.0f);
-            }
-            else if (Input.GetKeyUp(KeyCode.Alpha9) || Input.GetKeyUp(KeyCode.Keypad9))
-            {
-                RpcSpawnInfo("Thank you for you participation!", 2.0f);
+                for (int i = 0; i < 9; i++)
+                {
+                    if (Input.GetKeyDown(alpha[i]) || Input.GetKeyDown(keypad[i]))
+                    {
+                        if (message != null)
+                            Destroy(message);
+
+                        GameObject InfoCanvas = Instantiate(InfoCanvasPrefab, new Vector3(0f, 3.5f, -4.0f), Quaternion.identity);
+                        InfoCanvas.GetComponentInChildren<Text>().text = input[i].text;
+                        message = InfoCanvas;
+
+                        RpcSpawnInfo(input[i].text);
+                    }
+                }
+
             }
         }
 
         [ClientRpc]
-        private void RpcSpawnInfo(string info, float delay)
+        private void RpcSpawnInfo(string info)
         {
-            GameObject InfoCanvas = Instantiate(NetworkManager.singleton.spawnPrefabs[0], new Vector3(0f, 1.7f, 0f), startPosition.rotation);
+            if (message != null)
+            {
+                Destroy(message);
+                message = null;
+            }
+
+            if (info == "Delete")
+                return;
+
+            GameObject InfoCanvas = Instantiate(InfoCanvasPrefab, new Vector3(0f, 1.7f, 0f), startPosition.rotation);
             InfoCanvas.GetComponentInChildren<Text>().text = info;
-            Destroy(InfoCanvas, delay);
+            message = InfoCanvas;
         }
     }
 }
